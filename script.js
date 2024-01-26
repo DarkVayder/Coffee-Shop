8fff8a668b929c059b7c0039a30ea61a4b77f1a1
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-01-26 02:18:43 +0100

    script

---
+++ script.js
@@ -0,0 +1,11 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let section = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+    })
+}
